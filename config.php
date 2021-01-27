ba899f9e69a164dcdbe302d968d2ab1b6c32c7c6
jb-tech1999 <65624057+jb-tech1999@users.noreply.github.com>
2021-01-27 12:22:21 +0200

    register system added

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
     //Adding database credentials
 
-    $link = mysqli_connect('localhost', 'root', '' , 'cattle_manager');
+   // $link = mysqli_connect('localhost', 'root', '' , 'cattle_manager');
+    $mysqli = new mysqli('localhost', 'root', '' , 'cattle_manager');
 
     //check connection
-    if ($link === false){
+    if ($mysqli === false){
         die("Error: Could not connect. " . mysqli_connect_error());
     }
 
